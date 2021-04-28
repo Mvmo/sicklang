09b2bdd14d65e94fe5911f814c9c9d541fdd6ab4
Mvmo <mvmogo@gmail.com>
2021-04-28 21:45:03 +0200

    Add ast

---
+++ parser/src/main/java/dev/mvmo/sicklang/parser/ast/AbstractSyntaxTree.java
@@ -0,0 +1,11 @@
+package dev.mvmo.sicklang.parser.ast;
+
+import dev.mvmo.sicklang.parser.ast.node.statement.StatementNode;
+
+import java.util.List;
+
+public class AbstractSyntaxTree {
+
+    private List<StatementNode> statements;
+
+}
