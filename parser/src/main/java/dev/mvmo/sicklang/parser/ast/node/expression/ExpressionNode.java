4b3a4ac9b706dfae9dd01391139ce4d426a24baa
Mvmo <mvmogo@gmail.com>
2021-04-28 21:21:34 +0200

    Add expression-, program- and statement node

---
+++ parser/src/main/java/dev/mvmo/sicklang/parser/ast/node/expression/ExpressionNode.java
@@ -0,0 +1,6 @@
+package dev.mvmo.sicklang.parser.ast.node.expression;
+
+import dev.mvmo.sicklang.parser.ast.node.Node;
+
+public interface ExpressionNode extends Node {
+}
---
+++ parser/src/main/java/dev/mvmo/sicklang/parser/ast/node/statement/StatementNode.java
@@ -0,0 +1,6 @@
+package dev.mvmo.sicklang.parser.ast.node.statement;
+
+import dev.mvmo.sicklang.parser.ast.node.Node;
+
+public interface StatementNode extends Node {
+}
