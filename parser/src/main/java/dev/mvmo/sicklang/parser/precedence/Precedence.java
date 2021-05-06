e03f720844d1c07f4d1241b142d6af9c5ca44134
Mvmo <mvmogo@gmail.com>
2021-05-06 05:01:08 +0200

    Apply precedence to specific tokens

--- parser/src/main/java/dev/mvmo/sicklang/parser/precedence/Precedence.java
+++ parser/src/main/java/dev/mvmo/sicklang/parser/precedence/Precedence.java
@@ -1,13 +1,32 @@
 package dev.mvmo.sicklang.parser.precedence;
 
+import com.google.common.collect.Sets;
+import dev.mvmo.sicklang.token.TokenType;
+
+import java.util.Arrays;
+import java.util.Set;
+
 public enum Precedence {
 
-    LOWEST,
-    EQUALS, // ==
-    LESS_GREATER_THAN, // > or <
-    SUM, // +
-    PRODUCT, // *
+    LOWEST(),
+    EQUALS(TokenType.EQUALS, TokenType.NOT_EQUALS), // ==
+    LESS_GREATER_THAN(TokenType.LESS_THAN, TokenType.GREATER_THAN), // > or <
+    SUM(TokenType.PLUS, TokenType.MINUS), // +
+    PRODUCT(TokenType.SLASH, TokenType.ASTERISK), // *
     PREFIX, // -x or !x
-    CALL // myFunction(x)
+    CALL; // myFunction(x)
+
+    private final Set<TokenType> appliedToTypes;
+
+    Precedence(TokenType... appliedTo) {
+        this.appliedToTypes = Sets.newHashSet(appliedTo);
+    }
+
+    public static Precedence findPrecedence(TokenType tokenType) {
+        return Arrays.stream(values())
+                .filter(precedence -> precedence.appliedToTypes.contains(tokenType))
+                .findFirst()
+                .orElse(LOWEST);
+    }
 
 }
