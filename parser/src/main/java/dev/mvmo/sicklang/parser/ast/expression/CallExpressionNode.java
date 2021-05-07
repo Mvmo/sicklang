6df6fd3782798f39d3b264c53f262a0f78b1d25a
Mvmo <mvmogo@gmail.com>
2021-05-07 09:45:05 +0200

    Add CallExpressionNode

---
+++ parser/src/main/java/dev/mvmo/sicklang/parser/ast/expression/CallExpressionNode.java
@@ -0,0 +1,33 @@
+package dev.mvmo.sicklang.parser.ast.expression;
+
+import dev.mvmo.sicklang.token.Token;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+@AllArgsConstructor(staticName = "newInstance")
+public class CallExpressionNode implements ExpressionNode {
+
+    private final Token token;
+    private ExpressionNode function; // identifier or function
+    private List<ExpressionNode> arguments;
+
+    @Override
+    public String tokenLiteral() {
+        return token.literal();
+    }
+
+    @Override
+    public String toString() {
+        return function.toString() +
+                "(" +
+                arguments.stream().map(ExpressionNode::toString).collect(Collectors.joining(", ")) +
+                ")";
+    }
+
+}
