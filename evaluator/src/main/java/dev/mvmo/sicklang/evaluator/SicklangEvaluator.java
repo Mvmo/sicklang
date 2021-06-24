b687671f48672cd7236795a1d4e4e3e743695a73
Mvmo <mvmogo@gmail.com>
2021-06-24 03:39:44 +0200

    Implement evaluator

--- evaluator/src/main/java/dev/mvmo/sicklang/evaluator/SicklangEvaluator.java
+++ evaluator/src/main/java/dev/mvmo/sicklang/evaluator/SicklangEvaluator.java
@@ -1,4 +1,41 @@
 package dev.mvmo.sicklang.evaluator;
 
+import dev.mvmo.sicklang.internal.object.SickObject;
+import dev.mvmo.sicklang.internal.object.number.IntegerObject;
+import dev.mvmo.sicklang.parser.ast.Node;
+import dev.mvmo.sicklang.parser.ast.expression.IntegerLiteralExpressionNode;
+import dev.mvmo.sicklang.parser.ast.program.ProgramNode;
+import dev.mvmo.sicklang.parser.ast.statement.ExpressionStatementNode;
+import dev.mvmo.sicklang.parser.ast.statement.StatementNode;
+
+import java.util.List;
+
 public class SicklangEvaluator {
+
+    public static SickObject eval(Node node) {
+        if (node instanceof ProgramNode programNode) {
+            return evalStatements(programNode.statementNodes());
+        }
+
+        if (node instanceof ExpressionStatementNode expressionStatementNode) {
+            return eval(expressionStatementNode.expressionNode());
+        }
+
+        if (node instanceof IntegerLiteralExpressionNode integerNode) {
+            return new IntegerObject(integerNode.value());
+        }
+
+        return null;
+    }
+
+    private static SickObject evalStatements(List<StatementNode> statementNodes) {
+        SickObject result = null;
+
+        for (StatementNode statementNode : statementNodes) {
+            result = eval(statementNode);
+        }
+
+        return result;
+    }
+
 }
