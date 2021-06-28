a5a31d4bc1c8c960f53224404b90859cd948aebe
Mvmo <mvmogo@gmail.com>
2021-06-28 23:20:08 +0200

    Improve env

--- internal/src/main/java/dev/mvmo/sicklang/internal/env/SickEnvironment.java
+++ internal/src/main/java/dev/mvmo/sicklang/internal/env/SickEnvironment.java
@@ -1,10 +1,39 @@
 package dev.mvmo.sicklang.internal.env;
 
 import com.google.common.collect.Maps;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Map;
 
-public record SickEnvironment(Map<String, Object> storage) {
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class SickEnvironment {
+
+    private final Map<String, Object> storageMap;
+
+    public SickEnvironment set(String key, Object value) {
+        this.storageMap.put(key, value);
+        return this;
+    }
+
+    public <T> T setAndGet(String key, T value) {
+        set(key, value);
+        return value;
+    }
+
+    public Object get(String key) {
+        return storageMap.get(key);
+    }
+
+    public Object getOrDefault(String key, Object defaultValue) {
+        if (hasKey(key))
+            return get(key);
+        return defaultValue;
+    }
+
+    public boolean hasKey(String key) {
+        return storageMap.containsKey(key);
+    }
 
     public static SickEnvironment newInstance() {
         return new SickEnvironment(Maps.newHashMap());
