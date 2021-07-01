d28fa0ce456f4cd544d2db87aa5c0f456404e86c
Mvmo <mvmogo@gmail.com>
2021-07-01 17:37:05 +0200

    Add getter to array node

--- parser/src/main/java/dev/mvmo/sicklang/parser/ast/expression/ArrayLiteralExpressionNode.java
+++ parser/src/main/java/dev/mvmo/sicklang/parser/ast/expression/ArrayLiteralExpressionNode.java
@@ -1,11 +1,13 @@
 package dev.mvmo.sicklang.parser.ast.expression;
 
 import dev.mvmo.sicklang.token.Token;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Getter
 @RequiredArgsConstructor(staticName = "newInstance")
 public class ArrayLiteralExpressionNode implements ExpressionNode {
 
