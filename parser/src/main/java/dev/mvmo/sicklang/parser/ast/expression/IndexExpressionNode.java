bdb6fa46922354d8629258593427df61d74bc6fc
Mvmo <mvmogo@gmail.com>
2021-07-03 02:38:14 +0200

    @Getter for index node

--- parser/src/main/java/dev/mvmo/sicklang/parser/ast/expression/IndexExpressionNode.java
+++ parser/src/main/java/dev/mvmo/sicklang/parser/ast/expression/IndexExpressionNode.java
@@ -1,8 +1,10 @@
 package dev.mvmo.sicklang.parser.ast.expression;
 
 import dev.mvmo.sicklang.token.Token;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor(staticName = "newInstance")
 public class IndexExpressionNode implements ExpressionNode {
 
